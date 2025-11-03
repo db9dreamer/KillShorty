dff94c6be643a16c23bac0ac7299d3ed8b09bfd8
db9dreamer <dav1dsm1th@hotmail.com>
2025-11-03 21:04:57 +0000

    forgot to remove dead code

--- content.js
+++ content.js
@@ -1,13 +1,3 @@
-//function removeShorts(source) {
-//	const nodes = document.querySelectorAll('[is-shorts]');
-//	console.log(`Found:${nodes.length} state: ${document.readyState} source: ${source}`);
-//	nodes.forEach(node => { node.outerHTML = ''; });
-//}
-
-//document.addEventListener('DOMContentLoaded', removeShorts('DOMContentLoaded'));
-//document.addEventListener('yt-navigate-finish', removeShorts('yt-navigate-finish'));
-
-
 const observer = new MutationObserver(() => {
 	const nodes = document.querySelectorAll('[is-shorts]');
 	nodes.forEach(node => { node.outerHTML = ''; });
