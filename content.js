39d0abc736c9323fc184873f9a3c6aa9f39f63de
db9dreamer <dav1dsm1th@hotmail.com>
2026-02-24 20:12:56 +0000

    Incremented version to figure out why extension wasn't nunning.  Turns out it needed "Insecure content" to be allowed - which Chrome had turned off randomly...

--- content.js
+++
@@ -1,9 +0,0 @@
-const observer = new MutationObserver(() => {
-	const nodes = document.querySelectorAll('[is-shorts]');
-	nodes.forEach(node => { node.outerHTML = ''; });
-});
-
-observer.observe(document.body, {
-	childList: true,
-	subtree: true
-});
